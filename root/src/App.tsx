41a752f5b99750176f1a9a033919c6602d378a51
RedDotz20 <carlos_tabangay@outlook.com>
2022-12-12 16:39:13 +0800

    feat: currency api

--- root/src/App.tsx
+++ root/src/App.tsx
@@ -1,8 +1,23 @@
-import React from "react";
-import "./App.css";
+import { useState } from "react";
+import "./App.scss";
+// https://app.freecurrencyapi.com/dashboard
+
+function getCurrencies() {
+	fetch("https://api.freecurrencyapi.com/v1/latest", {
+		headers: {
+			apikey: "gwRkxLlgKJx36K810uCTKfZjz8ZsLqnGDPqDp0FH",
+		},
+	})
+		.then((res) => res.json())
+		.then((data) => console.log(data.data));
+}
 
 function App() {
-	return <div className="App"></div>;
+	return (
+		<div className="App">
+			<button onClick={getCurrencies}>G E T C U R R E N C Y</button>
+		</div>
+	);
 }
 
 export default App;
