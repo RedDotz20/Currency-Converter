ceb624bbf5a01d8c48961e54b19f787f2c865765
RedDotz20 <carlos_tabangay@outlook.com>
2022-12-17 21:59:50 +0800

    feat: map values modified

--- root/src/App.tsx
+++ root/src/App.tsx
@@ -1,41 +1,33 @@
-import "./App.scss";
-
 import { useEffect, useState } from "react";
-
 import { Currency } from "./components/Currency";
 import { CurrencyType } from "./components/Currency";
 import axios from "axios";
+import "./App.scss";
 
 // https://app.freecurrencyapi.com/dashboard
 
-function App() {
-  const [currencies, setCurrencies] = useState<CurrencyType | {}>({});
+export default function App() {
+	const [currencies, setCurrencies] = useState<CurrencyType | {}>({});
+	const getCurrencies = async () => {
+		const response = await axios.get(
+			"https://api.freecurrencyapi.com/v1/latest",
+			{
+				headers: {
+					apikey: "gwRkxLlgKJx36K810uCTKfZjz8ZsLqnGDPqDp0FH",
+				},
+			}
+		);
+		setCurrencies(() => response.data.data);
+	};
 
-  const getCurrencies = async () => {
-    const response = await axios.get(
-      "https://api.freecurrencyapi.com/v1/latest",
-      {
-        headers: {
-          apikey: "gwRkxLlgKJx36K810uCTKfZjz8ZsLqnGDPqDp0FH",
-        },
-      }
-    );
-    setCurrencies(() => response.data.data);
-  };
+	useEffect(() => {
+		getCurrencies();
+	}, []);
 
-  useEffect(() => {
-    getCurrencies();
-  }, []);
-
-  console.log("Currencies");
-  console.log(currencies);
-
-  return (
-    <div className="App">
-      <button onClick={() => getCurrencies()}>G E T C U R R E N C Y</button>
-      <Currency currencies={currencies} />
-    </div>
-  );
+	return (
+		<div className="App">
+			<button onClick={() => getCurrencies()}>G E T C U R R E N C Y</button>
+			<Currency currencies={currencies} />
+		</div>
+	);
 }
-
-export default App;
--- root/src/components/Currency.tsx
+++ root/src/components/Currency.tsx
@@ -1,27 +1,26 @@
 export interface CurrencyType {
-  string: number;
+	string: number;
 }
 
 export function Currency(props: { currencies: CurrencyType | {} }) {
-  return (
-    <table>
-      <thead>
-        <tr>
-          <td>Currency</td>
-          <td>Value</td>
-        </tr>
-      </thead>
-      <tbody>
-        {Object.entries(props.currencies).map((key, value) => {
-          console.log(key);
-          return (
-            <tr>
-              <td>{key}</td>
-              <td>{value}</td>
-            </tr>
-          );
-        })}
-      </tbody>
-    </table>
-  );
+	return (
+		<table>
+			<thead>
+				<tr>
+					<td>Currency</td>
+					<td>Value</td>
+				</tr>
+			</thead>
+			<tbody>
+				{Object.entries(props.currencies).map((value) => {
+					return (
+						<tr>
+							<td>{value[0]}</td>
+							<td>{value[1]}</td>
+						</tr>
+					);
+				})}
+			</tbody>
+		</table>
+	);
 }
