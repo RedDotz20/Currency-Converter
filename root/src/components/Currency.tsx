8b96b1a67c812cf9c126fb387cbe62100de5402b
RedDotz20 <carlos_tabangay@outlook.com>
2022-12-21 16:15:44 +0800

    feat: value conversion

--- root/src/components/Currency.tsx
+++ root/src/components/Currency.tsx
@@ -1,28 +1,46 @@
 import { useState } from "react";
+import { currencyInterface } from "../interface/currencyType";
 
-export function Currency(props: { currencies: CurrencyType | {} }) {
-	const [base, setBase] = useState(null);
-	const [convertTo, setConvertTo] = useState(null);
+export function Currency(props: currencyInterface) {
+	const [base, setBase] = useState<number>(0);
+	const [convertTo, setConvertTo] = useState<number>(0);
+	const [value, setValue] = useState<number | null>(null);
+
+	console.log("Base:", typeof base, "ConvertTo:", typeof convertTo);
+	console.log(props.currencies);
 
 	return (
-		<form>
-			<label htmlFor="base">Base</label>
-			<select name="base" id="base">
-				{Object.entries(props.currencies).map((value, key) => {
-					return <option value={value[0]}>{value[0]}</option>;
-				})}
-			</select>
+		<>
+			<h2>{base}</h2>
+			<h2>{convertTo}</h2>
+			<h1>{base * convertTo}</h1>
+			<form>
+				<label htmlFor="base">Base</label>
+				<select
+					name="base"
+					id="base"
+					onChange={(e) => setBase(parseFloat(e.target.value))}
+				>
+					{Object.entries(props.currencies).map((value, key) => {
+						return <option value={value[1]}>{value[0]}</option>;
+					})}
+				</select>
 
-			<label htmlFor="convertTo">Convert To</label>
-			<select name="convertTo" id="convertTo">
-				{Object.entries(props.currencies).map((value) => {
-					return <option value={value[0]}>{value[0]}</option>;
-				})}
-			</select>
-		</form>
+				<label htmlFor="convertTo">Convert To</label>
+				<select
+					name="convertTo"
+					id="convertTo"
+					onChange={(e) => setConvertTo(parseFloat(e.target.value))}
+				>
+					{Object.entries(props.currencies).map((value) => {
+						return <option value={value[1]}>{value[0]}</option>;
+					})}
+				</select>
+			</form>
+		</>
 	);
 }
 
-export interface CurrencyType {
-	string: number;
-}
+// export interface CurrencyType {
+// 	string: number;
+// }
---
+++ root/src/interface/currencyType.ts
@@ -0,0 +1,3 @@
+export interface currencyInterface {
+	currencies: { string: number } | {};
+}
