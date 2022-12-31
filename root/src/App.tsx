77b94f3af0bf6cbdc8ef63038efb4363d0144cdc
RedDotz20 <carlos_tabangay@outlook.com>
2022-12-31 16:05:16 +0800

    refactor: useFetchAPI reconfigure

--- root/src/App.tsx
+++ root/src/App.tsx
@@ -1,21 +1,20 @@
-import Currency from "./components/Currency";
+// import Currency from "./components/Currency";
 import useFetchAPI from "./hooks/useFetchAPI";
 
 export default function App() {
 	const { data, error, loading } = useFetchAPI();
 	if (loading)
 		return <h1 className="text-white grid place-items-center">Loading</h1>;
-	if (error)
-		return <h3 className="text-white grid place-items-center">{error}</h3>;
+	if (error) return error;
 
-	// console.log(typeof Object.keys(data));
+	console.log(data);
 
 	return (
 		<div className="p-8 h-full flex flex-col justify-center text-center">
 			<h1 className="text-white font-bold tracking-widest mb-8">
 				Currency Converter
 			</h1>
-			<Currency currencies={data} />
+			{/* <Currency currencies={data} /> */}
 		</div>
 	);
 }
--- root/src/hooks/useFetchAPI.tsx
+++ root/src/hooks/useFetchAPI.tsx
@@ -1,24 +1,42 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+interface keyValuesInterface {
+	currency: string;
+	value: number;
+}
+
+interface errorDataInterface {
+	message: string;
+	code: number;
+	details: object;
+}
+
 export default function useFetchAPI() {
-	// const [data, setData] = useState<{ string: number } | {}>({});
-	const [data, setData] = useState<any>(null);
-	const [error, setError] = useState<any>(null);
+	const [data, setData] = useState<keyValuesInterface[]>([]);
+	const [error, setError] = useState<errorDataInterface | unknown>(null);
 	const [loading, setLoading] = useState<boolean>(true);
 
 	useEffect(() => {
 		async function fetchData() {
 			try {
 				const baseURL = "https://api.freecurrencyapi.com/v1/latest";
-				const response = await axios.get(baseURL, {
-					headers: {
-						apikey: "gwRkxLlgKJx36K810uCTKfZjz8ZsLqnGDPqDp0FH",
-					},
-				});
-				setData(() => response.data.data);
-			} catch (error) {
-				setError(() => error);
+				const response = await axios
+					.get(baseURL, {
+						headers: {
+							apikey: "gwRkxLlgKJx36K810uCTKfZjz8ZsLqnGDPqDp0FH",
+						},
+					})
+					.then((response) => {
+						setData(() =>
+							Object.entries(response.data.data).map(([currency, value]) => ({
+								currency: currency as string,
+								value: value as number,
+							}))
+						);
+					});
+			} catch (err) {
+				setError(err);
 			} finally {
 				setLoading(() => false);
 			}
